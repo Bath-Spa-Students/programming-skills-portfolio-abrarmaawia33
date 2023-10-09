dc5aac0013c6f6b615a2f9abadaea6db13b8846b
abrarmaawia33 <144321058+abrarmaawia33@users.noreply.github.com>
2023-10-09 12:12:20 +0400

    Practice7.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Practice7.py
@@ -0,0 +1,11 @@
+val=15
+print(f'the value is {val+15}')
+name= "noha"
+print(f'the name is {name}')
+f_name= "abrar"
+s_name= "noha"
+name=f_name+ s_name
+print(name )
+name = "abrar"
+print(f'my name is {name}')
+      
