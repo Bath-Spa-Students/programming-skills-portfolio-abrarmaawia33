a44963ef0b8de54214efd1a812dbd020660e5b7d
abrarmaawia33 <144321058+abrarmaawia33@users.noreply.github.com>
2023-10-21 20:01:23 +0400

    Ch3_exercise6.py

---
+++ Chapter 3- Data Structures/Exercises/Ch3_exercise6.py
@@ -0,0 +1,66 @@
+guests = ['aryam', 'shahad', 'masara', 'noha' , 'reyam']
+
+name = guests[0].title()
+print(name + ", I would like to invite you for a dinner.")
+
+name = guests[1].title()
+print(name + ",I would like to invite you for a dinner .")
+
+name = guests[2].title()
+print(name + ", I would like to invite you for a dinner.")
+
+name = guests[3].title()
+print(name + ", I would like to invite you for a dinner.")
+
+name = guests[4].title()
+print(name + ", I would like to invite you for a dinner.")
+
+
+name = guests[0].title()
+print("\nSorry, " + name + " can't make it to dinner.")
+
+del(guests[0])
+guests.insert(0, 'mayada')
+
+name= guests[0].title()
+print(name + ",I would like to invite you for dinner .")
+
+name = guests[1].title()
+print(name + ",I would like to invite you for a dinner .")
+
+name = guests[2].title()
+print(name + ", I would like to invite you for a dinner.")
+
+name = guests[3].title()
+print(name + ", I would like to invite you for a dinner.")
+
+name = guests[4].title()
+print(name + ", I would like to invite you for a dinner.")
+
+
+print("\nSorry, we can only invite two people to dinner.")
+
+
+name = guests.pop()
+print("Sorry, " + name.title() + " there's no place on the dinner table.")
+
+name = guests.pop()
+print("Sorry, " + name.title() + " there's no place on the dinner table.")
+
+name = guests.pop()
+print("Sorry, " + name.title() + " there's no place on the dinner table.")
+
+
+
+name = guests[0].title()
+print(name + ", please come to dinner.")
+
+name = guests[1].title()
+print(name + ", please come to dinner.")
+
+
+del(guests[0])
+del(guests[0])
+
+
+print(guests)
